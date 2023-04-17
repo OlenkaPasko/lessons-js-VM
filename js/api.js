e5aa731db4aeaca81660ec9546e437f23fca74ff
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-04-17 21:31:04 +0300

    start

---
+++ js/api.js
@@ -0,0 +1,53 @@
+const API_KEY = "900c3c2e823d6abe3929dac959f94e63";
+const BASE_URL = "https://api.themoviedb.org/3";
+const TREND_URL = "${BASE_URL}/trending/movi";
+const SEARCH_URL = "${BASE_URL}/search/movi";
+const ID_URL = "${BASE_URL}/movi";
+
+export { API_KEY, BASE_URL, TREND_URL, SEARCH_URL, ID_URL };
+
+import axios from "axios";
+import { API_KEY, BASE_URL, TREND_URL, SEARCH_URL, ID_URL } from "./api";
+
+export default {
+  async getTrendData(page) {
+    try {
+      const { data } = await axios.get(
+        `${TREND_URL}?api_key=${API_KEY}&page=${page}`
+      );
+      return data;
+    } catch (error) {
+      console.error("Smth wrong with api get full trends" + error);
+    }
+  },
+};
+
+export async function getTrending(page = 1) {
+  const url = `${BASE_URL}/trending/all/day?api_key=${API_KEY}&language=en-US&page=${page}`;
+  return await axios
+    .get(url)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => console.log(error));
+}
+
+async function fetchMovieSearcher(text, page) {
+  try {
+    const { data } = await axios.get(
+      `${SEARCH_URL}?api_key=${API_KEY}&query=${text}&page=${page}`
+    );
+    return data;
+  } catch (error) {
+    console.error("Smth wrong with api search fetch" + error);
+  }
+}
+
+async function getMovieById(id) {
+  try {
+    const { data } = await axios.get(`${ID_URL}${id}?api_key=${API_KEY}`);
+    const result = {};
+  } catch (error) {
+    console.error("Smth wrong with api ID fetch" + error);
+  }
+}
--- js/asynchFn.js
+++ js/asynchFn.js
@@ -1,7 +1,7 @@
 //promise
-[1, 2, 3, 4].map((element, index, array)=> {
+//[1, 2, 3, 4].map((element, index, array)=> {
     
-});//джс так влаштований, що як тільки map і колбек він має
+//});//джс так влаштований, що як тільки map і колбек він має
 //доситуп до element, index, array(масива).Точно по такій схемі працює проміс
 
 //const promise = new Promise((resolve, reject) => {
@@ -16,16 +16,16 @@
 не обов'зково.СетТймаут щоб імітувати затримку в коді
 Бувають частини оду довго виконуються. асинхрн.щоб швидше працювало*/
 
-let total = 0;
-for (let i = 0; i < 1000000000000; i++) {
-    total += 1;
-}//довго працюватиме в браузері.
+//let total = 0;
+//for (let i = 0; i < 1000000000000; i++) {
+    //total += 1;
+//}//довго працюватиме в браузері.
 
-const result = new Promise((resolve, reject) => {
-  let total = 0;
-  for (let i = 0; i < 1000000000000; i++) {
-    total += 1;
-  }
-});
-console.log(result);
+//const result = new Promise((resolve, reject) => {
+  //let total = 0;
+  //for (let i = 0; i < 1000000000000; i++) {
+    //total += 1;
+  //}
+//});
+//console.log(result);
 //так задопомого. проміс швидше
